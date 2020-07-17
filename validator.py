5bfd7b9881d9965ade30d4f0bbfbe1e0212c1fa3
Wayne W.J. Chen <uijin.tw@gmail.com>
2020-07-17 23:53:07 +0000

    Introduce logging; refine validator data structure.

--- validator.py
+++ validator.py
@@ -3,18 +3,20 @@ import requests
 import telegram
 import time
 import json
+import logging
 
 # Load data from config.ini file
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-validator = {
-    '31441': {
-        'url': 'https://beaconcha.in/dashboard/data/validators?validators=31441',
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+log = logging.getLogger(__name__)
+
+validator_state = {
+    'https://beaconcha.in/dashboard/data/validators?validators=31441': {
         'active': True
     },
-    '4224': {
-        'url': 'https://altona.beaconcha.in/dashboard/data/validators?validators=4224',
+    'https://altona.beaconcha.in/dashboard/data/validators?validators=4224': {
         'active': True
     }
 }
@@ -24,16 +26,20 @@ if __name__ == '__main__':
     bot = telegram.Bot(token=config['TELEGRAM']['ACCESS_TOKEN'])
     bot.sendMessage(chat_id=config['CHAT']['ID'], text=f'Validator monitor start\n{json.dumps(validator, indent=2)}')
     while True:
-        for k, v in validator.items():
-            #print(k, v)
-            res = requests.get(v['url'])
-            data = res.json()['data'][0]
-            print(data[1], data[3])
+        for url in validator_state:
+            res = requests.get(url)
+            try:
+                data = res.json()['data'][0]
+            except JSONDecodeError as e:
+                log.exception(res.text)
+                continue
+            log.info('%s, %s',data[1], data[3])
             index = data[1] # == k
             state = data[3]
-            if (state == 'active_online') == validator[index]['active']:
+            if (state == 'active_online') == validator_state[url]['active']:
                 continue
-            message = f'Validator {index} state change to {active}'
+            message = f'Validator {index} state change to {state}'
             bot.sendMessage(chat_id=config['CHAT']['ID'], text=message)
+            validator_state[url]['active'] = not validator_state[url]['active'] 
         time.sleep(3)
 
