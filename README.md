0b45579cdc6419d0e0f3ba16983c27058d31fae5
Wayne W.J. Chen <uijin.tw@gmail.com>
2020-08-04 15:50:22 +0000

    Use YAML for configuration; Refine code.

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # eth2-validator-monitor
 
-Add `config.ini` file with content include
+Rename `config-example.yaml` to `config.yaml`, modify configuration.
 ```
-[TELEGRAM]
-ACCESS_TOKEN = {your bot access token}
-
-[CHAT]
-ID = {your chat id. Reference: https://www.freecodecamp.org/news/telegram-push-notifications-58477e71b2c2/}
+TELEGRAM:
+  ACCESS_TOKEN: "{your bot access token}"
+CHAT:
+  ID: {your chat id. Reference: https://www.freecodecamp.org/news/telegram-push-notifications-58477e71b2c2/}
+validator_url:
+- "https://beaconcha.in/dashboard/data/validators?validators={validator_index_1,validator_index_2}
 ```
--- validator.py
+++ validator.py
@@ -1,26 +1,19 @@
-import configparser
 import requests
 import telegram
 import time
 import json
 import logging
 import atexit
-
-# Load data from config.ini file
-config = configparser.ConfigParser()
-config.read('config.ini')
+import yaml
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 log = logging.getLogger(__name__)
 
-validator_state = {
-    'https://beaconcha.in/dashboard/data/validators?validators=31441': {
-        'active': True
-    },
-    'https://altona.beaconcha.in/dashboard/data/validators?validators=4224': {
-        'active': True
-    }
-}
+with open('config.yaml', 'r') as f:
+    config = yaml.safe_load(f)
+
+validator_url = config['validator_url']
+validator_active = {}
 
 @atexit.register
 def say_goodbye():
@@ -30,24 +23,28 @@ def say_goodbye():
 if __name__ == '__main__':
     # print(config['TELEGRAM']['ACCESS_TOKEN'])
     bot = telegram.Bot(token=config['TELEGRAM']['ACCESS_TOKEN'])
-    bot.sendMessage(chat_id=config['CHAT']['ID'], text=f"`Validator` monitor *start*\n```{json.dumps(validator_state, indent=2)}```", parse_mode='MarkdownV2')
+    bot.sendMessage(chat_id=config['CHAT']['ID'], text=f"`Validator` monitor *start*\n```{json.dumps(validator_url, indent=2)}```", parse_mode='MarkdownV2')
     while True:
-        for url in validator_state:
+        for url in validator_url:
             res = requests.get(url)
             try:
-                data = res.json()['data'][0]
-            except json.decoder.JSONDecodeError as e:
+                res_json = res.json()
+            except json.decoder.JSONDecodeError:
                 log.exception(res.text)
                 continue
-            log.info('%s, %s',data[1], data[3])
-            index = data[1] # == k
-            state = data[3]
-            if (state == 'active_online') == validator_state[url]['active']:
+            try:
+                for data in res_json['data']:
+                    log.info('%s, %s',data[1], data[3])
+                    index = data[1] # == k
+                    state = data[3]
+                    if (state == 'active_online') == validator_active.get(index, True):
+                        continue
+                    message = f'<b>{index}</b> change to {state}'
+                    message = message.replace('active_online', 'active_online👍')
+                    message = message.replace('active_offline', 'active_offline🗿')
+                    bot.send_message(chat_id=config['CHAT']['ID'], text=message, parse_mode='HTML')
+                    validator_active[index] = not validator_active[index]
+            except IndexError:
+                log.exception(res.text)
                 continue
-            message = f'<b>{index}</b> change to {state}'
-            message = message.replace('active_online', 'active_online👍')
-            message = message.replace('active_offline', 'active_offline🗿')
-            bot.send_message(chat_id=config['CHAT']['ID'], text=message, parse_mode='HTML')
-            validator_state[url]['active'] = not validator_state[url]['active'] 
         time.sleep(5)
-
